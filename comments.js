b324cd5d36844422ebd234429011bf39aac897f0
Evgenii Prusov <114025336+evgenii-prusov@users.noreply.github.com>
2024-06-08 18:30:32 +0000

    copilot third commit #2

--- comments.js
+++ comments.js
@@ -1,71 +1,14 @@
 // create web server
-// start server
-// create a route for comments
-// create a route for comments/:id
-// create a route for comments/:id/delete
-// create a route for comments/:id/edit
+var http = require('http');
+var fs = require('fs');
 
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/comments', (req, res) => {
-    res.send('Comments Index');
+// create server
+var server = http.createServer(function(req, res) {
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/comments.html', 'utf8');
+  myReadStream.pipe(res);
 });
 
-app.get('/comments/:id', (req, res) => {
-    res.send(`Comment Show Page for id: ${req.params.id}`);
-});
-
-app.get('/comments/:id/delete', (req, res) => {
-    res.send(`Comment Delete Page for id: ${req.params.id}`);
-});
-
-app.get('/comments/:id/edit', (req, res) => {
-    res.send(`Comment Edit Page for id: ${req.params.id}`);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-}); 
-// run server: node comments.js
-// open browser: http://localhost:3000/comments
-// open browser: http://localhost:3000/comments/1
-// open browser: http://localhost:3000/comments/1/delete
-// open browser: http://localhost:3000/comments/1/edit
-
-// Path: comments.js
-// create web server
-// start server
-// create a route for comments
-// create a route for comments/:id
-// create a route for comments/:id/delete
-// create a route for comments/:id/edit
-
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/comments', (req, res) => {
-    res.send('Comments Index');
-});
-
-app.get('/comments/:id', (req, res) => {
-    res.send(`Comment Show Page for id: ${req.params.id}`);
-});
-
-app.get('/comments/:id/delete', (req, res) => {
-    res.send(`Comment Delete Page for id: ${req.params.id}`);
-});
-
-app.get('/comments/:id/edit', (req, res) => {
-    res.send(`Comment Edit Page for id: ${req.params.id}`);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-}); 
-// run server: node comments.js
-// open browser: http://localhost:3000/comments
-// open browser: http://localhost:3000/comments/1
-// open
+// listen to port 3000
+server.listen(3000);
